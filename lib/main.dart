a5518322ab3a0f0ba8cff35b7e584ba6aa3b2a6b
Khalid Ramadan <khalid.waleed.ra@gmail.com>
2024-09-10 11:51:12 +0300

    additional steps

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:news_app_ui_setup/views/home_view.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: "api.env");
   runApp(const NewsApp());
 }
 
